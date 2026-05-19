1e906b75500e940b24f82bd1e03aed5669209e1b
Hadiqa <hadiqaimrannn0@gmail.com>
2026-05-19 20:49:09 +0300

    fixed minor bugs

--- app.py
+++ app.py
@@ -1,44 +1,86 @@
 from flask import Flask, render_template, request, redirect, session, url_for
+import sqlite3
+import os
+print(os.path.abspath("users.db"))
 
 app = Flask(__name__)
 app.secret_key = "dev_secret_key_change_later" #so this is for session management
 
 
 #first trying with a list approach 
-users = {}
+
 @app.route("/") #this is the main page 
 def home():
-   if "users" in session:
+   if "user" in session:
    	return redirect(url_for("dashboard")) #used to send user to a specific page, it generates a url for "dashboard" function
    return redirect(url_for("login"))
    
    
 @app.route("/register", methods=["GET", "POST"])
 def register():
-	if request.method == "POST":
-		username = request.form["username"]
-		password = request.form["password"]
-		
-		if username in users:
-			return "USER ALREADY EXISTS"
-		
-		users[username] = password
-		return redirect(url_for("dashboard"))
-	return render_template("register.html")
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect("users.db")
+        cursor = conn.cursor()
+
+        # check if user exists
+        cursor.execute(
+            "SELECT * FROM users WHERE username = ?",
+            (username,)
+        )
+
+        existing_user = cursor.fetchone()
+
+        if existing_user:
+            conn.close()
+            return "USER ALREADY EXISTS"
+
+        
+        cursor.execute(
+            "INSERT INTO users (username, password) VALUES (?, ?)",
+            (username, password)
+        )
+
+        conn.commit()
+        conn.close()
+
+        session["user"] = username
+
+        return redirect(url_for("dashboard"))
+
+    return render_template("register.html")
 	
 @app.route("/login", methods=["GET", "POST"])
-
 def login():
-	if request.method == "POST":
-		username = request.form["username"]
-		password = request.form["password"]
-		
-		if username in users and users[username] == password:
-			session["user"] = username
-			return redirect(url_for("dashboard"))
-		return "invalid credentials"
-		
-	return render_template("login.html")
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect("users.db")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "SELECT * FROM users WHERE username = ? AND password = ?",
+            (username, password)
+        )
+
+        user = cursor.fetchone()
+
+        conn.close()
+
+        if user:
+            session["user"] = username
+            return redirect(url_for("dashboard"))
+
+        return "invalid credentials"
+
+    return render_template("login.html")
 	
 @app.route("/logout")
 def logout():
